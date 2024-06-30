d14767cca74aa6b75fd3bb7af92d10581bcb36e8
xxcosita3czxx <kokosove18@gmail.com>
2024-06-30 23:36:04 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,11 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 curl -Ls https://raw.githubusercontent.com/xxcosita3czxx/coldhost-vps/main/install.sh -o install.sh
 chmod +x ./install.sh
-sudo service cron start
+chmod +x /usr/local/bin/traffic_monitor.sh
+# Set up the cron job
+(crontab -l 2>/dev/null; echo "* * * * * /usr/local/bin/traffic_monitor.sh") | crontab -
+
+# Ensure cron is started
+service cron start
 # Run the VPS Installer
 sh ./install.sh
